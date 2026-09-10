6df3d5a0308d8841295a6f5de676a723a92a2f95
davemt74-lang <davemt74@gmail.com>
2026-09-10 08:24:50 -0700

    Section 4: install and migrate canonical plugin lifecycle

--- setup.php
+++ setup.php
@@ -29,6 +29,7 @@ try {
     vp3_plugin_ensure_schema_v320($pdo);
     vp3_social_ensure_schema_v320($pdo);
     artist_workspace_v181_ensure_schema($pdo);
+    vp3_plugin_migrate_legacy_v360($pdo);
     music_workspace_release_schema_v330_ensure($pdo);
     music_workspace_resources_v330_ensure_schema($pdo);
     password_reset_ensure_schema();
