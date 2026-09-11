56da5480c46f0a9874329ed780d2d6a2f33d2708
davemt74-lang <davemt74@gmail.com>
2026-09-11 14:48:27 -0700

    Load generic commerce appointment adapter

--- includes/agent-paid-appointments-v800.php
+++ includes/agent-paid-appointments-v800.php
@@ -10,3 +10,4 @@ require_once __DIR__.'/agent-paid-appointments-v800-part2.php';
 require_once __DIR__.'/agent-paid-appointments-v800-part3.php';
 require_once __DIR__.'/agent-paid-appointments-v800-part4.php';
 require_once __DIR__.'/agent-paid-appointments-v800-part5.php';
+require_once __DIR__.'/agent-commerce-appointment-adapter-v800.php';
