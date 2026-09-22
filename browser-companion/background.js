173c6cd97c5727b3a04046500890b825f953f6e7
davemt74-lang <davemt74@gmail.com>
2026-09-21 22:53:05 -0700

    Report Browser Companion v22.9.0

--- browser-companion/background.js
+++ browser-companion/background.js
@@ -1,6 +1,6 @@
 const VP3_DEFAULT_BASE = 'https://vp3.me';
 const VP3_CONTRACT_VERSION = '1';
-const VP3_EXTENSION_VERSION = '22.8.0';
+const VP3_EXTENSION_VERSION = '22.9.0';
 const VP3_MEDIA_CLIP_MAX_SECONDS = 90;
 
 const storage = {
