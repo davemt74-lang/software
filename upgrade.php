40188b381214f19724a9d6a03f0610b6bdc2bbc7
davemt74-lang <davemt74@gmail.com>
2026-09-25 11:23:16 -0700

    Install and require v2.4 Contacts mutation schema on upgrade

--- upgrade.php
+++ upgrade.php
@@ -76,6 +76,7 @@ function vp3_upgrade_complete(): bool
         && homeserver_https_v1300_schema_ready()
         && homeserver_shared_v210_schema_ready()
         && homeserver_federated_v240_schema_ready()
+        && homeserver_contacts_v241_schema_ready()
         && homeserver_execution_v230_schema_ready()
         && homeserver_scheduling_v620_schema_ready()
         && homeserver_commerce_agent_v1000_schema_ready()
@@ -284,6 +285,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             homeserver_https_v1300_ensure_schema($pdo);
             homeserver_shared_v210_ensure_schema($pdo);
             homeserver_federated_v240_ensure_schema($pdo);
+            homeserver_contacts_v241_ensure_schema($pdo);
             homeserver_execution_v230_ensure_schema($pdo);
             chrome_extension_releases_ensure_schema($pdo);
             client_release_rollouts_ensure_schema_v110($pdo);
